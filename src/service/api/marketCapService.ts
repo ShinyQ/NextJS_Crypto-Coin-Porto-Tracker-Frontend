a492c94aab19a8f96740400b5b0481800d249499
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2024-03-14 02:55:23 +0700

    feat: fetch using axios

--- src/service/api/marketCapService.ts
+++ src/service/api/marketCapService.ts
@@ -1,11 +1,22 @@
+import axios from 'axios';
+
 export async function fetchCoinData(symbols: string): Promise<any> {
+    const url = `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=${symbols}`;
+    
     try {
-        const response = await fetch(
-        `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=a5eb2931-37db-4757-ab07-f7b1e02cbed5&symbol=${symbols}`
-        );
-        const { data } = await response.json();
+        const response = await axios.get(url, {
+            headers: {
+                'X-CMC_PRO_API_KEY': 'a5eb2931-37db-4757-ab07-f7b1e02cbed5',
+            },
+        });
+
+        if (response.status !== 200) {
+            throw new Error("Failed to fetch data. Response status: " + response.status);
+        }
+        const { data } = response.data
         return data;
     } catch (error) {
+        console.error("Error fetching data:", error);
         throw new Error("Error fetching data");
     }
 }
