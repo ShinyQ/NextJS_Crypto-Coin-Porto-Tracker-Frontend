2952a433dcbefc0ac39bb5b1f8712257f18cbc76
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2024-03-14 03:29:43 +0700

    feat: change url using backend api

--- src/service/api/marketCapService.ts
+++ src/service/api/marketCapService.ts
@@ -1,20 +1,16 @@
 import axios from 'axios';
 
 export async function fetchCoinData(symbols: string): Promise<any> {
-    const url = `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=${symbols}`;
+    const url = `https://coin-porto-tracker-be-three.vercel.app/api/coin-data?symbols=${symbols}`;
     
     try {
-        const response = await axios.get(url, {
-            headers: {
-                'X-CMC_PRO_API_KEY': 'a5eb2931-37db-4757-ab07-f7b1e02cbed5',
-            },
-        });
+        const response = await axios.get(url);
 
         if (response.status !== 200) {
             throw new Error("Failed to fetch data. Response status: " + response.status);
         }
-        const { data } = response.data
-        return data;
+
+        return response.data;
     } catch (error) {
         console.error("Error fetching data:", error);
         throw new Error("Error fetching data");
