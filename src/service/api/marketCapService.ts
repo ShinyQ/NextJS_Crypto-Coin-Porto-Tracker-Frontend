53b9b9fe951bd2417c55fe2b98d709639cbb4b67
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2024-04-03 03:54:03 +0700

    feat: add coins and use fetch instead of axios

--- src/service/api/marketCapService.ts
+++ src/service/api/marketCapService.ts
@@ -1,16 +1,17 @@
-import axios from 'axios';
-
 export async function fetchCoinData(symbols: string): Promise<any> {
     const url = `https://coin-porto-tracker-be-three.vercel.app/api/coin-data?symbols=${symbols}`;
-    
-    try {
-        const response = await axios.get(url);
 
-        if (response.status !== 200) {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            cache: 'no-store'
+        });
+
+        if (!response.ok) {
             throw new Error("Failed to fetch data. Response status: " + response.status);
         }
 
-        return response.data;
+        return await response.json();
     } catch (error) {
         console.error("Error fetching data:", error);
         throw new Error("Error fetching data");
