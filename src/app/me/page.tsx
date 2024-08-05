35c1cfe73be9840a75a37bc447791aa92e669ffc
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2024-08-06 00:15:23 +0700

    change base porto

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center">
       <h3 className="font-bold text-2xl m-5 pt-3">Portofolio Tracker</h3>
-      <CoinList data={coin} startPortofolio={85000000} />
+      <CoinList data={coin} startPortofolio={82000000} />
     </main>
   );
 }
