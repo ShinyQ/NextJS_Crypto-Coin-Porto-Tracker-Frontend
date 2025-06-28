f992138c7798ebdd6c93d4561c4343a880010430
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2025-06-28 21:42:35 +0700

    update BTC u1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-background">
       <h3 className="font-bold text-2xl m-5 pt-3 text-foreground">Portfolio Tracker</h3>
-      <CoinList data={coin} startPortfolio={100000000} />
+      <CoinList data={coin} startPortfolio={80000000} />
     </main>
   );
 }
